c924a30ee5da62da058f997354fa3140ee24b755
NicholasTng01 <e0969528@u.nus.edu>
2023-05-23 12:26:19 +0800

    Navbar and Logo created.

---
+++ src/navbar/NavBar.js
@@ -0,0 +1,39 @@
+import Logo from "../Resources/logoSample.png";
+import './navbarStyle.css';
+
+function NavBar () {
+    return (
+        <>
+            <body>
+                <nav className="navbar">
+                    {/* Logo */}
+                    <img className="logo" src={Logo} alt="WealthWatch" />
+
+                    {/* Navigation Menu */}
+                    <ul className="nav-links">
+
+                        <div className="menu">
+
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/">About</a></li>
+
+                            <li className="services">
+                                <a href="/">Services</a>
+
+                                <ul className="dropdown">
+                                    <li><a href="/">Log Transaction </a></li>
+                                    <li><a href="/">Stock Price</a></li>
+                                </ul>
+
+                            </li>
+
+                            <li><a href="/">Contact</a></li>
+                        </div>
+                    </ul>
+                </nav>
+            </body>
+        </>
+    );
+}
+
+export default NavBar;
