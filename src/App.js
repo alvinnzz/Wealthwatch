20320010efd99ba002a5c0a3d36d72d235a7de58
NicholasTng010601 <nicholastng@hotmail.com>
2023-05-28 10:34:43 +0800

    editted transaction page css

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App(props) {
       <div className="App">
         <ChakraProvider>
           <NavBar />
-          <RegisterPage />
+          <FinanceTrackerPage />
         </ChakraProvider>
       </div>
     </>
