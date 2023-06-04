e1ed60f8c014be901fe6ff70df48d592cd6a6183
NicholasTng010601 <nicholastng@hotmail.com>
2023-06-04 19:12:25 +0800

    Donut graph financial tracker

--- src/App.js
+++ src/App.js
@@ -6,24 +6,50 @@ import RegisterPage from "./Pages/LoginPage/RegisterPage";
 import HomePage from "./Pages/HomePage/HomePage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
+import DonutGraph from "./Pages/ExpenseTracker/DonutGraph";
+// import useUserId from "./Components/useUserId";
+
+// function setUserId(userId) {
+//   sessionStorage.setItem("userId", JSON.stringify(userId));
+// }
+
+// function getUserId() {}
 
 function App(props) {
   const [loggedin, setLoggedin] = useState(false);
+  // const [userId, setUserId] = useState(null);
+  // const { userId, setUserId } = useUserId();
+  // const userId = getUserId();
+
+  // `  if (!userId) {
+  //     return (
+  //       <ChakraProvider>
+  //         <Router>
+  //           <LoginPage setLoggedin={setLoggedin} setUserId={setUserId} />
+  //         </Router>
+  //       </ChakraProvider>
+  //     );
+  //   }`
+
   return (
     <ChakraProvider>
       <Router>
-        <NavBar loggedin={loggedin} setLoggedin={setLoggedin} />
-        <HomePage />
+        <NavBar loggedin={loggedin} setLoggedin={setLoggedin} userId={userId} />
+        <HomePage userId={userId} />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route
             path="/login"
-            element={<LoginPage setLoggedin={setLoggedin} />}
+            element={
+              <LoginPage setLoggedin={setLoggedin} setUserId={setUserId} />
+            }
           />
-          <Route path="/financialtracker" element={<FinanceTrackerPage />} />
+          <Route path="/financialtracker" element={<DonutGraph />} />
+          {/* <Route path="/financialtracker" element={<FinanceTrackerPage />} /> */}
         </Routes>
       </Router>
+      {/* <DonutGraph /> */}
     </ChakraProvider>
   );
 }
