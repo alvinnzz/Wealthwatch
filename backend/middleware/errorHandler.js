7fc6fe0ad1d50392a35e91db56fea0aec0d46f14
alvinnzz <alvinng01@live.com>
2023-06-30 11:42:01 +0800

    resolve json syntax error

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,5 +1,6 @@
 const errorHandler = (err, req, res, next) => {
-    return res.status(400).send(err.message);
+    //return res.status(400).send(err.message);
+    return res.status(400).json({ error: err.message });
 }
 
 module.exports = errorHandler;
